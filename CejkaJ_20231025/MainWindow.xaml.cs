f2158cde91334a01b41ceac3d24ced2b95326c8f
Jan Čejka <Cejka.Jan@stud.vassboskovice.cz>
2023-10-25 13:08:32 +0200

    hotovo

--- CejkaJ_20231025/MainWindow.xaml.cs
+++ CejkaJ_20231025/MainWindow.xaml.cs
@@ -20,9 +20,112 @@ namespace CejkaJ_20231025
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool Shape { get; set; }
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent();            
+            checkshape();
         }
+        //ukol 2
+        public void checkshape()
+        {
+            if (cb.SelectedIndex == 0)
+            {
+                thirdSite.Visibility = Visibility.Visible;
+                thirdSiteName.Visibility = Visibility.Visible;
+                digit.Visibility = Visibility.Visible;
+                Shape = true;
+            }
+            else
+            {
+                thirdSiteName.Visibility = Visibility.Collapsed;
+                thirdSite.Visibility = Visibility.Collapsed;
+                digit.Visibility = Visibility.Collapsed;
+                Shape = false;
+            }
+        }
+        public double calc(bool shape)
+        {
+            double vysledek = 0;
+            double a = double.Parse(firstSite.Text);
+            double b = double.Parse(secondSite.Text);            
+            if (shape == true)
+            {
+                double c = double.Parse(thirdSite.Text);
+                double meziobsah = (a + b+c)/2.0;
+                double mezivysledek = meziobsah * (meziobsah-a) * (meziobsah-b) * (meziobsah-c);
+                vysledek = Math.Sqrt(mezivysledek);
+                vysledek = Math.Round(vysledek, 2);
+                return vysledek;
+            }
+            else
+            {
+                vysledek = a * b;
+                return vysledek;
+            }
+        }
+
+        private void cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            checkshape();
+        }
+
+        private void CalcBtn_Click(object sender, RoutedEventArgs e)
+        {            
+            if (Shape == true)
+            {
+                result.Text = "Obsah Trojuhelniku je "+ calc(Shape).ToString() + " cm ctverecnich";
+            }
+            else
+            {
+                result.Text = "Obsah Obdelniku je " + calc(Shape).ToString() + " cm ctverecnich";
+            }
+        }
+
+        //ukol 2
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int x = int.Parse(X.Text);
+            int y = int.Parse(Y.Text);
+            int n = int.Parse(N.Text);
+            generate(x,y,n-1);
+        }
+
+        public void generate(int x,int y,int n)
+        {
+            VysledekGrid.Children.Clear();
+            
+            for (int i = 0; i < x; i++)
+            {
+                
+                VysledekGrid.ColumnDefinitions.Add(new ColumnDefinition {Width = new GridLength(25)});                           
+                for (int j = 0; j < y; j++)
+                {
+                    if (j==n||i==n)
+                    {
+                        Rectangle mainrec = new Rectangle();
+                        mainrec.Fill = Brushes.Green;
+                        mainrec.Stroke = Brushes.Black;
+                        VysledekGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(25) });
+                        Grid.SetRow(mainrec, i);
+                        Grid.SetColumn(mainrec, j);
+                        VysledekGrid.Children.Add(mainrec);
+                    }
+                    else
+                    {
+                        Rectangle mainrec = new Rectangle();
+                        mainrec.Fill = Brushes.White;
+                        mainrec.Stroke = Brushes.Black;
+                        VysledekGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(25) });
+                        Grid.SetRow(mainrec, i);
+                        Grid.SetColumn(mainrec, j);
+                        VysledekGrid.Children.Add(mainrec);
+                    }
+                }
+            }
+            
+
+        }        
+
     }
 }
